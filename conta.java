6b6ae3eb0ba66ad3ed0edda619d83d6b23f92265
jose_alex <josealex41012@gmai.com>
2024-08-19 14:24:15 -0300

    adicionando_classe_conta

--- conta.java
+++ conta.java
@@ -1,1 +1,11 @@
-sfsfsf
+public class Conta {
+    public int numero;
+    public String titular_da_conta;
+    public double saldo_da_conta;
+
+    public void depositar(double saldo_da_conta, double valor_usuario, double resultado_adicionado){
+        resultado_adicionado = saldo_da_conta + valor_usuario;
+    }
+
+
+}
