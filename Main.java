a744df08e8b5d4d5cf42bd184da8199eebc9869b
jose_alex <josealex41012@gmai.com>
2024-08-26 13:35:27 -0300

    adicionando conta

--- Main.java
+++ Main.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class Main {
-    
+    public static void main (String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        Conta conta1 = new Conta(12223, "Ribeiro", 1000.00);
+
+        System.out.println("Digite o valor do deposito: ");
+        double valor_deosito = entrada.nextDouble();
+        conta1.depositar(valor_deosito);
+        conta1.extrato();
+
+        System.out.println("Digite o valor do saque: ");
+        double valor_saque = entrada.nextDouble();
+        conta1.sacar(valor_saque);
+        conta1.extrato();
+    }
 }
--- conta.java
+++ conta.java
@@ -1,11 +1,31 @@
 public class Conta {
-    public int numero;
+    private int numero;
     public String titular_da_conta;
     public double saldo_da_conta;
 
-    public void depositar(double saldo_da_conta, double valor_usuario, double resultado_adicionado){
-        resultado_adicionado = saldo_da_conta + valor_usuario;
+    public Conta(int numero, String titular_da_conta, double saldo_da_conta){
+        this.numero = numero;
+        this.titular_da_conta = titular_da_conta;
+        this.saldo_da_conta = saldo_da_conta;
     }
 
+    public void depositar(double valor_deposito) {
+        if (valor_deposito > 0) {
+            saldo_da_conta = saldo_da_conta += valor_deposito;
+        } else {
+            System.out.println("O seu deposito não foi finalizado!");
+        }
+    } 
 
+    public void sacar(double valor_deposito) {
+        if (valor_deposito > 0) {
+            saldo_da_conta = saldo_da_conta -= valor_deposito;
+        } else {
+            System.out.println("O seu saque não foi finalizado!");
+        }
+    }
+
+    public void extrato() {
+        System.out.println("O seu saldo é: " + saldo_da_conta);
+    }
 }
